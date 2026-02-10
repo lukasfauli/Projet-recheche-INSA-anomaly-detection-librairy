995b0f2641e1648629756242ade86ff4a2eefbb1
Yassine EL ASSAMI <yassine.el-assami@capgemini.com>
2026-02-10 16:59:22 +0100

    Preparing environment + start of data exploration (work01)

--- demos/demo_data_loader.py
+++ demos/demo_data_loader.py
@@ -1,4 +1,3 @@
-from loader import load_data
 import sys
 
 config_dir = "../config"
@@ -6,6 +5,7 @@ if config_dir not in sys.path:
     sys.path.insert(0, config_dir)
 
 from config import google_folder_id as id
+
 
 data_dict, extracted_dir = load_data(
     # common CSV options if needed:
--- src/data_loader.py
+++ src/data_loader.py
@@ -1,14 +1,14 @@
 import sys
 from pathlib import Path
 import tarfile
-# import gdown
+import gdown
 import pandas as pd
 
 config_dir = "../config"
 if config_dir not in sys.path:
     sys.path.insert(0, config_dir)
 
-from config import google_folder_id as file_id
+from config import google_folder_id as default_file_id
 
 
 def download_gdrive_tar(file_id: str, tar_path: str | Path) -> Path:
