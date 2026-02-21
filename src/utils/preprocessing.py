49af3656ce12ef1ffc1b92a869b598469c2991dd
lukasfauli <faulilukas@gmail.com>
2026-02-21 11:45:41 +0100

    modification après visio de preprocessing.py + test + création visualization.py

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -1,4 +1,4 @@
-# function to omit NAn values
+
 
 import pandas as pd
 
@@ -13,7 +13,7 @@ def gen_vprint(verbose):
 vprint = gen_vprint(verbose=True)
 
 
-def drop_duplicate(df, inplace=True) -> pd.DataFrame:
+def drop_duplicate(df, inplace=True,Verbose=False) -> pd.DataFrame:
     '''
     Remove all the duclicates from the dataframe df
     print a message withe the number of row removed, if there is no row removed there is no message
@@ -22,19 +22,20 @@ def drop_duplicate(df, inplace=True) -> pd.DataFrame:
     :rtype: DataFrame
     '''
     len_before = len(df)
-    df_cleaned = df.drop_duplicates(inplace=inplace)
-    rows_removed = len_before - len(df_cleaned)
+    df.drop_duplicates(inplace=inplace)
+    rows_removed = len_before - len(df)
 
-    # Print the number of rows removed, if any
-    if rows_removed > 0:
-        vprint(f"INFO: {rows_removed} lines have been removed.")
-    elif rows_removed == 0:
-        vprint("INFO: No duplicate lines have been found.")
+    # Print the number of rows removed, if any. False by default
+    if Verbose:
+        if rows_removed > 0:
+            vprint(f"INFO: {rows_removed} lines have been removed.")
+        elif rows_removed == 0:
+            vprint("INFO: No duplicate lines have been found.")
 
-    return df_cleaned
+    return df
 
 
-def drop_nan(df) -> pd.DataFrame:
+def drop_nan(df,inplace=True,Verbose=False) -> pd.DataFrame:
     '''
     remove all the lines containing a Nan from the dataframe df
     print a message withe the number of row removed, if there is no row removed there is no message
@@ -42,14 +43,18 @@ def drop_nan(df) -> pd.DataFrame:
     :return: the cleaned data_frame
     :rtype: DataFrame
     '''
-    # TODO: avoid data duplication for memory optimization
-    df_cleaned = df.dropna().copy()
-    rows_removed = len(df) - len(df_cleaned)
-    if rows_removed > 0:
-        print(f"INFO: {rows_removed} lines have been removed.")
-    elif rows_removed == 0:
-        print("INFO: No lines with NaN values have been found.")
-    return df_cleaned
+
+    len_before=len(df)
+    df.dropna(inplace=inplace)
+    rows_removed = len_before - len(df)
+
+    # Print the number of rows removed, if any. False by default
+    if Verbose:
+        if rows_removed > 0:
+            print(f"INFO: {rows_removed} lines have been removed.")
+        elif rows_removed == 0:
+            print("INFO: No lines with NaN values have been found.")
+    return df
 
 
 def convert_datetime(df, inplace=True, datetime_column="datetime") -> pd.DataFrame:
@@ -67,15 +72,22 @@ def convert_datetime(df, inplace=True, datetime_column="datetime") -> pd.DataFra
         df_cleaned[datetime_column], errors='coerce')
     return df_cleaned
 
+def renorm_time(df, datetime_column="datetime") -> pd.DataFrame:
+    '''
+    transform the datetime into time starting from 0 to help to plot the columns
+    it creates a new column at the end of the data frame
+    :param df: Description
+    :return: Description
+    :rtype: DataFrame
+    '''
+    if not datetime_column in df.columns:
+        raise KeyError("the datetime column doesn't exist ")
+    times=df[datetime_column]
+    first_time=df[datetime_column].iloc[0]
+    df['time_real'] = (times - first_time).dt.total_seconds()
+    return df
 
-# def convert_dict_to_dataframe(dictionnary) -> pd.DataFrame:
-#     '''
-#     Convert the data from dictionnary to dataframe
-#     :param dict: dictionnary
-#     :return: a dataframe
-#     :rtype: DataFrame
-#     '''
-#     return pd.DataFrame(dictionnary)
+
 
 
 def clean_dataframe(df, inplace=True) -> pd.DataFrame:
@@ -87,27 +99,14 @@ def clean_dataframe(df, inplace=True) -> pd.DataFrame:
     :rtype: DataFrame
     '''
     df_cleaned = df if inplace else df.copy()
+    convert_datetime(df_cleaned, inplace=inplace)
+    renorm_time(df, datetime_column="datetime")
     drop_duplicate(df_cleaned, inplace=inplace)
     drop_nan(df_cleaned, inplace=inplace)
-    convert_datetime(df_cleaned, inplace=inplace)
+    
+    
 
     return df_cleaned
 
 
-def preprocess_all_sessions(sessions_dict: dict) -> dict:
-    """
-    Apply the full preprocessing pipeline to multiple driving sessions.
-    This function iterates through a dictionary of sessions, applying all the previous cleaning function
-    to each session individually to maintain data 
-    integrity and avoid cross-contamination between different runs.
-    Args:
-    Returns:
-        dict: A dictionnary with the cleaned DataFrames.
-    """
-    cleaned_sessions = dict()
-    for session_name, df in sessions_dict.items():
-        print(f"Nettoyage de la session : {session_name}")
-        cleaned_df = clean_dataframe(df)
-        cleaned_sessions[session_name] = cleaned_df
 
-    return cleaned_sessions
