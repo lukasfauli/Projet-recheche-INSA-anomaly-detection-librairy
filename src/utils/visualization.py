e03a88530845a2bf9e3ea0a845edec0b0dba76f0
HOANG-Anh2004 <vietanhhoangbui@gmail.com>
2026-02-22 13:32:07 +0100

    visualisation 2.0

--- src/utils/visualization.py
+++ src/utils/visualization.py
@@ -4,29 +4,46 @@ from  matplotlib import pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-def plot(df, col_name_X,col_name_Y):
+def plot(df, col_name_X, col_name_Y, ax=None):
      '''
-     Plotting function 
+     Plotting function with optional axis to support subplots
      ''' 
 
-     plt.plot(df[col_name_X],df[col_name_Y])
-     plt.xlabel(f"Values of {col_name_X}")
-     plt.ylabel(f"Values of {col_name_Y}")
-     plt.title( f"{col_name_X} in function of {col_name_Y}")
-     plt.show()
+     created_fig = False
+     if ax is None:
+          fig, ax = plt.subplots()
+          created_fig = True
+
+     ax.plot(df[col_name_X], df[col_name_Y])
+     ax.set_xlabel(f"Values of {col_name_X}")
+     ax.set_ylabel(f"Values of {col_name_Y}")
+     ax.set_title(f"{col_name_Y} in function of {col_name_X}")
+
+     if created_fig:
+          plt.show()
     
-def plot_hist(df,col_name_X,bins=100):
+def plot_hist(df, col_name_X, bins=100, ax=None):
      '''
      Representation in the form of a histogram to see possible discontinuity in data
+     Accepts an optional `ax` to draw into an existing subplot.
      '''
-     plt.hist(df[col_name_X], bins=bins, color='skyblue', edgecolor='black')
-     plt.yscale("log")   #the use of the log scale is to flat the peaks and raise the small values.
-     plt.ylabel("Number of occurence (Log)")
-     plt.title(f"Discontinuty analysis of {col_name_X}")
-     plt.show()
+     created_fig = False
+     if ax is None:
+          fig, ax = plt.subplots()
+          created_fig = True
 
-def boxplot(df,col_name):
-     sns.boxplot(x=df[col_name])
-     plt.title(f"Boxplot Analysis of {col_name}")
-     plt.xlabel(f"Values of {col_name}")
+     ax.hist(df[col_name_X], bins=bins, color='skyblue', edgecolor='black')
+     ax.set_yscale("log")   # use log scale to flatten peaks and raise small values
+     ax.set_ylabel("Number of occurence (Log)")
+     ax.set_title(f"Discontinuty analysis of {col_name_X}")
+
+     if created_fig:
+          plt.show()
+
+def boxplot(df,col_name,message=True, rotation=0):
+     sns.boxplot(data=df[col_name])
+     if message:
+          plt.title(f"Boxplot Analysis of {col_name}")
+          plt.xlabel(f"Values of {col_name}")
+     plt.xticks(rotation=rotation)
      plt.show()
